01bfb8d1ff0c8095c56e32c9a157faa44189e0ef
orislasky <43013466+orislasky@users.noreply.github.com>
2019-05-20 21:47:57 +0300

    there is still a bunch of mistakes here
    there is still a bunch of mistakes here

---
+++ area.java
@@ -0,0 +1,22 @@
+package parkinLot;
+import java.util.*;
+public class area {
+	public static Scanner in=new Scanner (System.in);
+private  string colorOfArea;
+private int numTotalSpt;
+private int taknSpot ;
+ 
+
+
+public area (String colorOfArea , int numTotalSpt , int taknSPot )
+{
+	this.colorOfArea = colorOfArea;
+	this.numTotalSpt = numTotalSpt;
+	this.taknSpot = taknSpot;
+}
+
+
+
+
+
+}
